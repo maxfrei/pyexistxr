9c7499c4d4df2407f8430ed4e1fe0d3ed30dc2b8
Alexey Sviridov <sviridov84@gmail.com>
2011-10-05 00:39:40 +0400

    - add files. 	new file:   __init__.py 	new file:   setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name = "pyexistxr",
+      version = "0.01",
+      description = "eXist database proxy via XML-RPC",
+      author = "Sviridov Alexey",
+      author_email = "sviridov84@gmail.com",
+      url = "https://github.com/maxfrei/PyExistXR",
+      packages = ['pyexistxr'],
+      license = "GPL-2",
+      classifiers=(
+          'Development Status :: 2 - Pre-Alpha',
+          'Environment :: Console',
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Operating System :: POSIX',
+          'Operating System :: Unix',
+          'Programming Language :: Python',
+          )
+      )
